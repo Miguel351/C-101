87a8310e750f83ff85a1c46a6c2570063da57b57
Miguel351 <spectrum@superonline.com>
2022-03-20 01:41:40 +0300

    Add files via upload

---
+++ hatayonetimi.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace hatayonetimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          // hata ihtimali olabilecek kodu try içine yaz
+          try
+          {
+              Console.WriteLine("Bir sayı giriniz");
+              int sayi=Convert.ToInt32(Console.ReadLine());
+              Console.WriteLine("Girmiş olduğunuz sayı: "+ sayi);
+          }
+          // hata yakalandığında yapılacaklar buraya yazılır
+          catch (Exception ex)
+          {
+             Console.WriteLine("Hata: "+ ex.Message.ToString());
+
+          }
+          // Hata olsun olmasın bu blok çalışsın
+          /*finally
+          {
+            Console.WriteLine("işlem tamamlandı: ");
+          }
+          */
+
+          try
+          {
+              //int a=int.Parse(null);
+              //int a= int.Parse("test");
+              int a= int.Parse("-51200000000000");
+          }
+          catch (ArgumentNullException ex)
+          {
+              Console.WriteLine("Boş değer girdiniz");
+              Console.WriteLine(ex);
+          }
+
+        catch (FormatException ex)
+          {
+              Console.WriteLine("Veri tipi uygun değil");
+              Console.WriteLine(ex);
+          }
+
+          catch (OverflowException ex)
+          {
+              Console.WriteLine("Çok küçük yada çok büyük bir değer girdiniz.");
+              Console.WriteLine(ex);
+          }
+
+          finally{
+               Console.WriteLine("ders bitmiştir :) ");
+          }
+        }
+    }
+}
