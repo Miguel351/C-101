3b7184e874ea18efbd8475d1de502992c648a23e
Miguel351 <spectrum@superonline.com>
2022-03-20 19:52:06 +0300

    Add files via upload

---
+++ if-else-if.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace if_else_if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time=DateTime.Now.Hour;
+
+            if(time>=6 && time<11){
+                Console.WriteLine("Günaydın! " + DateTime.Now);
+            } else if (time<=18){
+                Console.WriteLine("İyi Günler! " + DateTime.Now);
+            } else {
+                Console.WriteLine("İyi Geceler! " + DateTime.Now);
+            }
+
+            // ternany kullanımı -1
+            // ? işareti ise anlamında kullanılıyor
+            string sonuc= time<=18 ? "iyi günler!" : "iyi geceler";
+            Console.WriteLine(sonuc);
+
+            // ternany kullanımı -2
+            // : işareti değilse anlamında kullanılıyor
+            // iç içe if kullanımı gibi kullanmak gerekirse
+
+            sonuc = time >=6 && time <11 ? "Günaydın" : time <=18 ? "iyi günler" : "iyi geceler";
+            Console.WriteLine(sonuc);
+
+
+        }
+    }
+}
