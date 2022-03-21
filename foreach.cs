51918cb80cec803c6e78c4e0945ecd8f2a134664
Miguel351 <spectrum@superonline.com>
2022-03-22 00:37:07 +0300

    Add files via upload

---
+++ foreach.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace while_foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // while
+            // 1 den başlayarak consol dan girilen sayıya kadar (sayı dahil) ortalama hesaplayıp console a yazdıran program
+            Console.WriteLine("Lütfen bir sayı girin");
+            double sayi = double.Parse(Console.ReadLine());
+            int sayac=1;
+            double toplam=0;
+            while (sayac<=sayi)
+            {
+                toplam+=sayac;
+                sayac++;
+            }
+            Console.WriteLine(toplam/sayi);
+
+            // 'a' dan 'z' ye kadar tüm harfleri consola a yazdır.
+            char character='a';
+            while (character<='z')
+            {
+                Console.Write(character);
+                character++;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("***** foreach *****");
+            string[]arabalar={"BWM","Ford","Toyota","Nissan"};
+
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+
+        }
+    }
+}
