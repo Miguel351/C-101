37894d989c9e4bcc567d0cccc2844590b5623d45
Miguel351 <spectrum@superonline.com>
2022-04-18 11:36:21 +0300

    Add files via upload

---
+++ sinifKavrami.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace sinif_kavrami
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Söz Dizimi
+            //  class SınıfAdı
+            //  {
+            //      [Erişim Belirleyici] [Veri Tipi] ÖzellikAdi;
+            //      [Erişim Belirleyici] [Geri Dönüş Tipi] MetotAdi([Parametre Listesi]);
+            //      {
+            //          // Metot Komutları    
+            //      }
+            //  }
+
+            // Erişim Belirleyiciler
+            // * Public
+            // * Private
+            // * Internal
+            // * Protected
+
+            Calisan calisan1 =new Calisan();
+            calisan1.Ad="Ayşe";
+            calisan1.Soyad="Kara";
+            calisan1.No=23425634;
+            calisan1.Departman="İnsan Kaynakları";
+            calisan1.CalisanBilgileri();
+            Console.WriteLine("*******************");
+
+            Calisan calisan2 =new Calisan();
+            calisan2.Ad="Deniz";
+            //calisan2.Soyad="Arda";
+            calisan2.No=25632541;
+            calisan2.Departman="Satın Alma";
+            calisan2.CalisanBilgileri(); // nesneye ait tüm özellikleri metoda aktarmış oluyoruz.
+
+
+        }
+    }
+
+    class Calisan{
+        public string Ad;
+        public string Soyad;
+        public int No;
+        public string Departman;
+
+        public void CalisanBilgileri(){
+            Console.WriteLine("Çalışanın Adı:{0}", Ad);
+            Console.WriteLine("Çalışanın Soyadı:{0}", Soyad);
+            Console.WriteLine("Çalışanın Numarası:{0}", No);
+            Console.WriteLine("Çalışanın Departmanı:{0}", Departman);
+        }
+    }
+}
