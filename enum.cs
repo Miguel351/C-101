355ebb68e154ce618dad859814f422fbd4ef6453
Miguel351 <spectrum@superonline.com>
2022-04-20 22:16:54 +0300

    Add files via upload

---
+++ enum.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace csharp_enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Gunler.Pazar);
+            Console.WriteLine((int)Gunler.Cumartesi);
+
+            int sicaklik = 32;
+
+            if (sicaklik <= (int)HavaDurumu.normal)
+            {
+                Console.WriteLine("Dışarıya çıkmak için havanın biraz daha ısınmasını bekleyin");
+            }
+            else if (sicaklik >= (int)HavaDurumu.sicak)
+            {
+                Console.WriteLine("Dışarıya çıkmak için çok sıcak bir gün");
+            }
+            else if (sicaklik >= (int)HavaDurumu.normal && sicaklik < (int)HavaDurumu.cokSicak)
+                Console.WriteLine("Haydi dışarıya çıkalım!");
+        }
+    }
+
+    enum Gunler
+    {
+        //Pazartesi=1, şeklinde de alternatif kullanımı var
+        Pazartesi,
+        Salı,
+        Çarşamba,
+        Perşembe,
+        Cuma = 23,
+        Cumartesi,
+        Pazar
+    }
+
+    enum HavaDurumu
+    {
+        soguk = 5,
+        normal = 20,
+        sicak = 25,
+        cokSicak = 35
+    }
+}
